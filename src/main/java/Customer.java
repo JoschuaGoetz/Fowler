b2b85aabc5563ac7d6584eab0b1d07e67117d618
JoschuaGoetz <joschua.goetz@gmail.com>
2020-05-12 11:30:58 +0200

    Update folder structure

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,3 +1,4 @@
+package main.java;
 
 import java.util.Enumeration;
 import java.util.Vector;
@@ -33,7 +34,7 @@ class Customer
         double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();
-        String result = "Rental Record for " + this.getName() + "\n";
+        String result = "main.java.Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements())
