4669dfb34a89a9b524515ebb40a6875cad3673d8
JoschuaGoetz <joschua.goetz@gmail.com>
2020-05-12 13:05:07 +0200

    Inline variable - Remove local varaible thisAmount

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -31,18 +31,15 @@ class Customer
 
         while (enum_rentals.hasMoreElements())
         {
-            double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
-            thisAmount = each.getAmount();
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == PriceCode.NEW_RELEASE) && each.getDaysRented() > 1)
                 frequentRenterPoints++;
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getAmount()) + "\n";
+            totalAmount += each.getAmount();
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
